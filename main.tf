c0ef71ab962b34472708805fdc1d93eda99b2d05
Leif Eriksen <leif.eriksen.au@gmail.com>
2020-03-30 19:09:24 +1100

    minimal changes to get local plan clean, need to fix sqldw module a little

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 0.12"
+
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "agl-experiment"
@@ -23,21 +25,21 @@ locals {
   subscription_id = "40371827-837f-4329-a4c1-1000a8a29725"
   location        = "australiasoutheast"
 
-  aad_admin = "24a210e1-db99-4fe1-a7f8-35d1d3813e26" # me
-  aad_admin_name ="AAD_ADMIN"
+  aad_admin      = "24a210e1-db99-4fe1-a7f8-35d1d3813e26" # me
+  aad_admin_name = "AAD_ADMIN"
 }
 
-
 provider "azurerm" {
-  tenant_id       = "${local.tenant_id}"
-  subscription_id = "${local.subscription_id}"
-  version         = "~> 1.0"
+  tenant_id       = local.tenant_id
+  subscription_id = local.subscription_id
+  version         = "~> 2.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "workspace-rg" {
-  location = "${local.location}"
+  location = local.location
   name     = "sqldw-rg"
-  tags     = "${local.tags}"
+  tags     = local.tags
 }
 
 provider "random" {
@@ -53,20 +55,20 @@ resource "random_string" "sqldw-password" {
 }
 
 module "sqldw" {
-  source  = "app.terraform.io/agl-experiment/sqldw/azurerm"
-  version = "1.0.0"
+  source  = "app.terraform.io/agl-experiment/tf12-sqldw/azurerm"
+  version = "1.4.0"
 
-  resource_group_name = "${azurerm_resource_group.workspace-rg.name}"
-  location = "${local.location}"
-  tags = "${local.tags}"
-  sql_dw_name = "sql-dw-name"
-  db_name = "db_name"
-  sql_admin_user_name = "dataplat"
-  sql_admin_password = "${random_string.sqldw-password.result}"
-  dwuname = "DW100c"
-  aad_admin_user_id = "${local.aad_admin}"
-  aad_admin_user_name = "${local.aad_admin_name}"
-  azure_tenant_id = "${local.tenant_id}"
+  resource_group_name  = azurerm_resource_group.workspace-rg.name
+  location             = local.location
+  tags                 = local.tags
+  sql_dw_name          = "sql-dw-name"
+  db_name              = "db_name"
+  sql_admin_user_name  = "dataplat"
+  sql_admin_password   = random_string.sqldw-password.result
+  dwuname              = "DW100c"
+  aad_admin_user_id    = local.aad_admin
+  aad_admin_user_name  = local.aad_admin_name
+  azure_tenant_id      = local.tenant_id
   whitelisted_networks = []
-  whitelisted_subnets = []
+  whitelisted_subnets  = []
 }
